89d6e064c86524a03a8cd6f81389797d97bd426e
Vedant Agarwala <vedant.kota@gmail.com>
2018-08-15 15:25:32 +0530

    Add the show action

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -20,6 +20,18 @@ class AssetsController < ApplicationController
     end
   end
 
+  def show
+    if @asset.uploaded?
+      render json: {
+        download_url: asset_url_creator.download_url(*download_url_params)
+      }
+    else
+      render json: {
+        error: "Cannot get url for status: #{@asset.status}"
+      }, status: :bad_request
+    end
+  end
+
 private
   def asset_url_creator
     @_asset_url_creator ||= AssetUrlCreator.new
@@ -27,5 +39,9 @@ private
 
   def set_asset
     @asset = Asset.find params[:id]
+  end
+
+  def download_url_params
+    [@asset.id, params[:timeout]&.to_i].compact
   end
 end
--- test/controllers/assets_controller_test.rb
+++ test/controllers/assets_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class AssetsControllerTest < ActionDispatch::IntegrationTest
   test 'should create upload_url on create' do
     asset_url_creator = Minitest::Mock.new
-    asset_url_creator.expect :upload_url, 'upload_url'
+    asset_url_creator.expect :upload_url, 'url'
 
     AssetUrlCreator.stub :new, asset_url_creator do
       assert_difference 'Asset.count' do
@@ -31,5 +31,40 @@ class AssetsControllerTest < ActionDispatch::IntegrationTest
     put asset_url asset, status: 'uploading'
     assert_response :bad_request
     assert asset.reload.uploading?
+  end
+
+  test 'should render error on show uploading asset' do
+    asset = assets :two
+    assert asset.uploading?
+
+    get asset_url asset, status: 'uploading'
+    assert_response :bad_request
+  end
+
+  test 'should create upload_url on show' do
+    asset = assets :one
+    asset_url_creator = Minitest::Mock.new
+    asset_url_creator.expect :download_url, 'url', [asset.id]
+
+    AssetUrlCreator.stub :new, asset_url_creator do
+      get asset_url asset
+    end
+
+    assert_mock asset_url_creator
+    assert_response :ok
+  end
+
+  test 'should create upload_url with timeout on show' do
+    asset = assets :one
+    timeout = 100
+    asset_url_creator = Minitest::Mock.new
+    asset_url_creator.expect :download_url, 'url', [asset.id, timeout]
+
+    AssetUrlCreator.stub :new, asset_url_creator do
+      get asset_url asset, timeout: timeout
+    end
+
+    assert_mock asset_url_creator
+    assert_response :ok
   end
 end
